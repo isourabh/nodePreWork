bfb41d6acc99b1f287879b13454d1fc59ffa2177
Sourabh Tripathi <Sourabh.Tripathi@walmart.com>
2016-09-05 21:45:33 +0530

    picking proxy url from command line

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
  */
 var http = require('http');
 let request = require('request');
-let destinationUrl = '127.0.0.1:8000';
-
+let argv = require('yargs').argv;
+let scheme = 'http://';
+let host = argv.host || '127.0.0.1';
+let port = argv.port || (host === '127.0.0.1' ? 8000 : 80);
+// Build the destinationUrl using the --host value
+let destinationUrl = scheme + host + ':' + port;
+console.log("Destination URL "+destinationUrl);
 var echoServer = http.createServer((request, response) => {
     for (let header in request.headers) {
         response.setHeader(header, request.headers[header])
@@ -15,14 +20,14 @@ echoServer.listen(8000);
 
 
 http.createServer((req,res) => {
-    process.stdout.write('\n\n\n' + JSON.stringify(req.headers))
+    process.stdout.write('\n\n\n' + JSON.stringify(req.headers));
     req.pipe(process.stdout);
     let options = {
         headers: req.headers,
-        url: `http://${destinationUrl}${req.url}`
+        url: `${destinationUrl}${req.url}`
     };
     options.method = req.method;
     var downstreamResponse = req.pipe(request(options));
     downstreamResponse.pipe(process.stdout);
-    downstreamResponse.pid(res);
-}).listen(9000);
+    downstreamResponse.pipe(res);
+}).listen(8001);
