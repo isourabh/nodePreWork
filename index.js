8ee60a26a73ccee3fdcf8f19e71bc06ade0c7b5a
Sourabh Tripathi <Sourabh.Tripathi@walmart.com>
2016-09-11 20:08:35 +0530

    added help documentation

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-/**
- * Created by stripa4 on 28/08/16.
- */
 var http = require('http');
 let request = require('request');
-let argv = require('yargs').argv;
+let argv = require('yargs')
+            .usage('Usage: node index.js [options]')
+    .example('node index.js -x google.com -p 80')
+    .alias('p', 'port')
+    .nargs('p', 1)
+    .describe('port', 'Specify a forwarding port')
+    .alias('x', 'host')
+    .nargs('x', 1)
+    .describe('host', 'Specify a forwarding host')
+    .alias('l', 'log')
+    .nargs('l', 1)
+    .describe('log', 'Specify a output log file')
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2016').argv;
 let scheme = 'http://';
 let host = argv.host || '127.0.0.1';
 let port = argv.port || (host === '127.0.0.1' ? 8000 : 80);
-// Build the destinationUrl using the --host value
-let destinationUrl = scheme + host + ':' + port;
-
+let destinationUrl = argv.url || scheme + host + ':' + port;
 let path = require('path');
 let fs = require('fs');
 let logPath = argv.log && path.join(__dirname, argv.log);
@@ -31,6 +40,9 @@ http.createServer((req,res) => {
         headers: req.headers,
         url: `${destinationUrl}${req.url}`
     };
+    if(req.headers['x-destination-url']){
+        options.url = req.headers['x-destination-url'];
+    }
     options.method = req.method;
     var downstreamResponse = req.pipe(request(options));
     downstreamResponse.pipe(process.stdout);
