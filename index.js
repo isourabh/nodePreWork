e1bf9bf79b837fb44bbc878e7adc837c9dc834f8
Sourabh Tripathi <Sourabh.Tripathi@walmart.com>
2016-09-11 16:10:31 +0530

    added --log option

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ let host = argv.host || '127.0.0.1';
 let port = argv.port || (host === '127.0.0.1' ? 8000 : 80);
 // Build the destinationUrl using the --host value
 let destinationUrl = scheme + host + ':' + port;
-console.log("Destination URL "+destinationUrl);
+
+let path = require('path');
+let fs = require('fs');
+let logPath = argv.log && path.join(__dirname, argv.log);
+let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout;
+
 var echoServer = http.createServer((request, response) => {
     for (let header in request.headers) {
         response.setHeader(header, request.headers[header])
@@ -20,8 +25,8 @@ echoServer.listen(8000);
 
 
 http.createServer((req,res) => {
-    process.stdout.write('\n\n\n' + JSON.stringify(req.headers));
-    req.pipe(process.stdout);
+    logStream.write('Request headers: ' + JSON.stringify(req.headers));
+    req.pipe(logStream, {end: false});
     let options = {
         headers: req.headers,
         url: `${destinationUrl}${req.url}`
